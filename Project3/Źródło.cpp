ababcb9842c283751630775d676e59e9d3da30ac
Kamil Świękała <kamil.swiekala@gmail.com>
2019-05-24 14:39:00 +0200

    Ćwiczenie 10 Spacje

---
+++ Project3/Źródło.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+int liczba_sp;
+string niewiadomo;
+void tekst();
+{
+	cout << "Napisz tekst: " << endl;
+	cin >> niewiadomo
+}
+void wypisz()
+{
+	if (niewiadomo == ' ')
+		liczba_sp++;
+
+}
+int main()
+{
+	tekst();
+	wypisz();
+
+		system("pause")
+		return 0
+}
